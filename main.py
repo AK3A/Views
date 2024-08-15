a26371511991d3b858e4dd296cc5d263c533ee13
AK3A <63219156+AK3A@users.noreply.github.com>
2024-08-15 18:27:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ with open('CAPTCHA.png', 'wb') as f:
 
 BYPASS_CAPTCHA = pytesseract.image_to_string("CAPTCHA.png")
 BYPASS_CAPTCHA = BYPASS_CAPTCHA.replace('\n', '')
-
+print(f"CAPTCHA: {BYPASS_CAPTCHA}")
 data = {
     form: BYPASS_CAPTCHA,
 }
@@ -145,6 +145,8 @@ def lol():
             response2 = s.post('https://zefoy.com/{}'.format(next_post_action), data = data).text
             base64_string2 = DECRYPTION_BASE64(response2)
 
+            print(base64_string2)
+            
             if 'Successfully 1000 views sent.' in str(base64_string2):
                 c+=1
                 print(f'Successfully 1000 views sent. - {c}', end = "\r")
